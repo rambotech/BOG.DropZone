f19a7e5411f5e23c8da61d3de253497ec2d32dfc
John J Schultz <johnmail@bitsofgenius.com>
2018-04-20 23:44:42 -0400

    add MD5 validation to payload and references; use BOG.SwissArmyKnife

--- src/BOG.DropZone.Client/RestApi.cs
+++ src/BOG.DropZone.Client/RestApi.cs
@@ -1,11 +1,13 @@
-﻿using BOG.DropZone.Client.Model;
+﻿using BOG.DropZone.Client.Helpers;
+using BOG.DropZone.Client.Model;
+using BOG.SwissArmyKnife;
 using System;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using BOG.DropZone.Client.Helpers;
 using System.Net;
 
 namespace BOG.DropZone.Client
@@ -41,7 +43,8 @@ namespace BOG.DropZone.Client
                     JsonConvert.SerializeObject(
                         new Lockbox
                         {
-                            Content = payload
+                            Content = payload,
+                            MD5 = Hasher.GetHashFromStringContent(payload, Encoding.UTF8, Hasher.HashMethod.MD5)
                         }),
                     Encoding.UTF8,
                     "application/json"));
@@ -55,6 +58,13 @@ namespace BOG.DropZone.Client
             if (response.StatusCode != HttpStatusCode.OK) throw new RestApiNonSuccessException(response.StatusCode);
 
             lockbox = Serializer<Lockbox>.FromJson(await response.Content.ReadAsStringAsync());
+            if (string.Compare(
+                lockbox.MD5, 
+                Hasher.GetHashFromStringContent(lockbox.Content, Encoding.UTF8, Hasher.HashMethod.MD5),
+                true) != 0)
+            {
+                throw new RestApiNonSuccessException(HttpStatusCode.Gone, "MD5 checksun failed");
+            }
             return lockbox.Content;
         }
 
@@ -65,7 +75,8 @@ namespace BOG.DropZone.Client
                     JsonConvert.SerializeObject(
                         new Lockbox
                         {
-                            Content = value
+                            Content = value,
+                            MD5 = Hasher.GetHashFromStringContent(value, Encoding.UTF8, Hasher.HashMethod.MD5)
                         }),
                     Encoding.UTF8,
                     "application/json"));
@@ -79,6 +90,13 @@ namespace BOG.DropZone.Client
             if (response.StatusCode != HttpStatusCode.OK) throw new RestApiNonSuccessException(response.StatusCode);
 
             lockbox = Serializer<Lockbox>.FromJson(await response.Content.ReadAsStringAsync());
+            if (string.Compare(
+                lockbox.MD5,
+                Hasher.GetHashFromStringContent(lockbox.Content, Encoding.UTF8, Hasher.HashMethod.MD5),
+                true) != 0)
+            {
+                throw new RestApiNonSuccessException(HttpStatusCode.Gone, "MD5 checksun failed");
+            }
             return lockbox.Content;
         }
 
