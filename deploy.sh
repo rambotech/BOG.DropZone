550842b9aec5a6aae55f5f440268af74f1583f2f
John J Schultz <johnmail@bitsofgenius.com>
2020-02-16 01:26:52 -0500

    happier path still

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ set -ev
 
 pwd
 
-echo $NUGET_API_KEY == hidden
-echo $NUGET_SOURCE == ${NUGET_SOURCE} 
-echo $TRAVIS_PULL_REQUEST == ${TRAVIS_PULL_REQUEST}
+echo NUGET_API_KEY
+echo NUGET_SOURCE == ${NUGET_SOURCE} 
+echo TRAVIS_PULL_REQUEST == ${$TRAVIS_PULL_REQUEST}
 
 dotnet build -c $BUILD_CONFIG ./src/BOG.DropZone.sln
 
