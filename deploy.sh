580d2b95a2adf7eee34f1618c301bfcd3c4bbd28
John J Schultz <johnmail@bitsofgenius.com>
2020-02-15 22:19:09 -0500

    syntax

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -ev
 
 dotnet build -c $BUILD_CONFIG ./src/BOG.DropZone.sln
-if [ "${TRAVIS_PULL_REQUEST}" = "false" && ${TRAVIS_BRANCH} = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master" ]; then
 		dotnet nuget push ./src/BOG.DropZone.Common/bin/$BUILD_CONFIG/BOG.DropZone.Common.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
 		dotnet nuget push ./src/BOG.DropZone.Client/bin/$BUILD_CONFIG/BOG.DropZone.Client.*.nupkg --api-key {NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
 fi
