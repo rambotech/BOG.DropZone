afad26415f8db7cefd5d3fdfc10ddaf9bf963bd1
John J Schultz <johnmail@bitsofgenius.com>
2020-04-30 13:32:19 -0400

    Initial (untested) support for LetsEncrypt

--- src/BOG.DropZone/Startup.cs
+++ src/BOG.DropZone/Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
-using Newtonsoft.Json;
-using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.IO;
 using System.Text.Json;
