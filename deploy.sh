7652c3f1eed20c895735c29127b61857f8fb5e1e
John J Schultz <johnmail@bitsofgenius.com>
2020-09-29 23:28:52 -0400

    script fix

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ echo BUILD_DIR == ${BUILD_DIR}
 dotnet build -c $BUILD_CONFIG ./src/BOG.DropZone
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-		dotnet nuget push ./src/BOG.DropZone.Common/bin/$BUILD_DIR/BOG.DropZone.Common.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
-		dotnet nuget push ./src/BOG.DropZone.Client/bin/$BUILD_DIR/BOG.DropZone.Client.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
+		dotnet nuget push ./src/BOG.DropZone.Common/bin/$BUILD_CONFIG/BOG.DropZone.Common.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
+		dotnet nuget push ./src/BOG.DropZone.Client/bin/$BUILD_CONFIG/BOG.DropZone.Client.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
 fi
