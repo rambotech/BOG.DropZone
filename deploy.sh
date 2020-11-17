22877cb05027fff824f9a8379ec6c97eed2a932d
John J Schultz <johnmail@bitsofgenius.com>
2020-11-16 20:56:50 -0500

    explicit solution name for build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ev
 
+cd src
 pwd
+ls -la
 
 echo NUGET_API_KEY ... contents hidden
 echo NUGET_SOURCE == ${NUGET_SOURCE} 
@@ -10,9 +12,9 @@ echo BUILD_CONFIG == ${BUILD_CONFIG}
 
 echo BUILD_DIR == ${BUILD_DIR}
 
-dotnet build -c $BUILD_CONFIG ./src/BOG.DropZone.sln
+dotnet build -c $BUILD_CONFIG BOG.DropZone
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-		dotnet nuget push ./src/BOG.DropZone.Common/bin/$BUILD_CONFIG/BOG.DropZone.Common.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
-		dotnet nuget push ./src/BOG.DropZone.Client/bin/$BUILD_CONFIG/BOG.DropZone.Client.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
+		dotnet nuget push ./BOG.DropZone.Common/bin/$BUILD_CONFIG/BOG.DropZone.Common.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
+		dotnet nuget push ./BOG.DropZone.Client/bin/$BUILD_CONFIG/BOG.DropZone.Client.*.nupkg --api-key ${NUGET_API_KEY} --source ${NUGET_SOURCE} --skip-duplicate
 fi
