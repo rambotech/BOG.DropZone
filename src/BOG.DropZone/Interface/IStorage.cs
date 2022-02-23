3fe919d82ed85ac461696da063429fea807a1678
John Schultz <johnmail@bitsofgenius.com>
2022-02-22 22:59:45 -0500

    debug of the blog (static reference)

--- src/BOG.DropZone/Interface/IStorage.cs
+++ src/BOG.DropZone/Interface/IStorage.cs
@@ -8,55 +8,98 @@ namespace BOG.DropZone.Interface
 	/// Defines the site's common storage items.
 	/// </summary>
 	public interface IStorage
-    {
-        /// <summary>
-        /// An optional access token value which the client must provide to use operational methods.
-        /// </summary>
-        string AccessToken { get; set; }
+	{
+		/// <summary>
+		/// Communicates that state for a blog key
+		/// </summary>
+		enum BlobState : int { 
+			/// <summary>
+			/// Key not found in the persistent storage
+			/// </summary>
+			NotFound = 0,
+			/// <summary>
+			/// The key exists, and the content is valid.
+			/// </summary>
+			Exists = 1,
+			/// <summary>
+			/// The key exists, but the content is expired.
+			/// </summary>
+			Expired = 2
+		};
 
-        /// <summary>
-        /// An optional access token value which the client must provide to use administrative methods.
-        /// </summary>
-        string AdminToken { get; set; }
+		/// <summary>
+		/// An optional access token value which the client must provide to use operational methods.
+		/// </summary>
+		string AccessToken { get; set; }
 
-        /// <summary>
-        /// The list of dropzone information
-        /// </summary>
-        int MaxDropzones { get; set; }
+		/// <summary>
+		/// An optional access token value which the client must provide to use administrative methods.
+		/// </summary>
+		string AdminToken { get; set; }
 
-        /// <summary>
-        /// The number of consecutive mismatched access token values which triggers a lockout period.
-        /// </summary>
-        int MaximumFailedAttemptsBeforeLockout { get; set; }
+		/// <summary>
+		/// The list of dropzone information
+		/// </summary>
+		int MaxDropzones { get; set; }
 
-        /// <summary>
-        /// The number of seconds which a lockout endures.
-        /// </summary>
-        int LockoutSeconds { get; set; }
+		/// <summary>
+		/// The number of consecutive mismatched access token values which triggers a lockout period.
+		/// </summary>
+		int MaximumFailedAttemptsBeforeLockout { get; set; }
 
-        /// <summary>
-        /// The list of dropzone information
-        /// </summary>
-        Dictionary<string, DropPoint> DropZoneList { get; set; }
+		/// <summary>
+		/// The number of seconds which a lockout endures.
+		/// </summary>
+		int LockoutSeconds { get; set; }
 
-        /// <summary>
-        /// The list of clients who have submitted invalid 
-        /// </summary>
-        List<ClientWatch> ClientWatchList { get; set; }
+		/// <summary>
+		/// The list of dropzone information
+		/// </summary>
+		Dictionary<string, DropPoint> DropZoneList { get; set; }
 
-        /// <summary>
-        /// Reset the site to a fresh startup state.
-        /// </summary>
-        void Reset();
+		/// <summary>
+		/// The list of clients who have submitted invalid 
+		/// </summary>
+		List<ClientWatch> ClientWatchList { get; set; }
 
-        /// <summary>
-        /// Reset only this dropzone to a fresh startup state.
-        /// </summary>
-        void Clear(string dropZoneName);
+		/// <summary>
+		/// Reads the content from a file, using the key as the file name.
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		BlobState ReadBlob(string zoneName, string key, out StoredValue value);
 
-        /// <summary>
-        /// Shutdown the site using an application exit.
-        /// </summary>
-        void Shutdown();
-    }
+		/// <summary>
+		/// Creates a file with the content, using the key as the file name
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		BlobState SaveBlob(string zoneName, string key, StoredValue value);
+
+		/// <summary>
+		/// Deletes a file with the content, using the key as the file name
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		void DeleteBlob(string zoneName, string key);
+
+		/// <summary>
+		/// Reset the site to a fresh startup state.
+		/// </summary>
+		void Reset();
+
+		/// <summary>
+		/// Reset only this dropzone to a fresh startup state.
+		/// </summary>
+		void Clear(string dropZoneName);
+
+		/// <summary>
+		/// Shutdown the site using an application exit.
+		/// </summary>
+		void Shutdown();
+	}
 }
--- src/BOG.DropZone/MemoryStorage.cs
+++ src/BOG.DropZone/MemoryStorage.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Timers;
 using BOG.DropZone.Common.Dto;
 using BOG.DropZone.Interface;
 using BOG.DropZone.Storage;
+using BOG.SwissArmyKnife.Extensions;
+using Microsoft.Extensions.Configuration;
+using BOG.SwissArmyKnife;
+using static BOG.DropZone.Interface.IStorage;
 
 namespace BOG.DropZone
 {
@@ -10,94 +17,177 @@ namespace BOG.DropZone
 	/// The collection of items persisted on the server, and static across calls.
 	/// </summary>
 	public class MemoryStorage : IStorage
-    {
-        readonly Timer stopTimer = new Timer();
-        readonly object lockPoint = new object();
+	{
+		readonly Timer stopTimer = new Timer();
+		readonly object lockPoint = new object();
 
-        /// <summary>
-        /// If not empty, the header value "AccessToken" from the client must contain this value to use the site.
-        /// </summary>
-        public string AccessToken { get; set; } = string.Empty;
+		/// <summary>
+		/// If not empty, the header value "AccessToken" from the client must contain this value to use the site.
+		/// </summary>
+		public string AccessToken { get; set; } = string.Empty;
 
-        /// <summary>
-        /// An optional access token value which the client must provide to use administrative methods.
-        /// </summary>
-        public string AdminToken { get; set; } = string.Empty;
+		/// <summary>
+		/// An optional access token value which the client must provide to use administrative methods.
+		/// </summary>
+		public string AdminToken { get; set; } = string.Empty;
 
-        /// <summary>
-        /// The maximum number of dropzones to allow.
-        /// </summary>
-        public int MaxDropzones { get; set; } = 10;
+		/// <summary>
+		/// The maximum number of dropzones to allow.
+		/// </summary>
+		public int MaxDropzones { get; set; } = 10;
 
-        /// <summary>
-        /// The maximum number of failed attempts before a lockout delay is invoked.
-        /// </summary>
-        public int MaximumFailedAttemptsBeforeLockout { get; set; } = 6;
+		/// <summary>
+		/// The maximum number of failed attempts before a lockout delay is invoked.
+		/// </summary>
+		public int MaximumFailedAttemptsBeforeLockout { get; set; } = 6;
 
-        /// <summary>
-        /// The duration of the lock out when imposed.
-        /// </summary>
-        public int LockoutSeconds { get; set; } = 600;
+		/// <summary>
+		/// The duration of the lock out when imposed.
+		/// </summary>
+		public int LockoutSeconds { get; set; } = 600;
 
-        /// <summary>
-        /// The collection of drop zones and their data.
-        /// </summary>
-        public Dictionary<string, DropPoint> DropZoneList { get; set; } = new Dictionary<string, DropPoint>();
+		/// <summary>
+		/// The collection of drop zones and their data.
+		/// </summary>
+		public Dictionary<string, DropPoint> DropZoneList { get; set; } = new Dictionary<string, DropPoint>();
 
-        /// <summary>
-        /// The list of clients who have submitted invalid 
-        /// </summary>
-        public List<ClientWatch> ClientWatchList { get; set; } = new List<ClientWatch>();
+		/// <summary>
+		/// The list of clients who have submitted invalid 
+		/// </summary>
+		public List<ClientWatch> ClientWatchList { get; set; } = new List<ClientWatch>();
 
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public MemoryStorage()
-        {
-            stopTimer.Enabled = false;
-            stopTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            stopTimer.Interval = 1000;
-        }
+		private readonly string PersistBaseFolder;
 
-        /// <summary>
-        /// Clear all drop zones, including their payloads and reference dictionary.
-        /// </summary>
-        public void Reset()
-        {
-            DropZoneList.Clear();
-            ClientWatchList.Clear();
-        }
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MemoryStorage(IConfiguration config)
+		{
+			PersistBaseFolder = ResolveLocalSpec(config.GetValue<string>("PersistFolderRoot", "$HOME/appdata/dropzone"));
+			if (!Directory.Exists(PersistBaseFolder))
+			{
+				Directory.CreateDirectory(PersistBaseFolder);
+			}
+			stopTimer.Enabled = false;
+			stopTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+			stopTimer.Interval = 1000;
+		}
 
-        /// <summary>
-        /// Clear a specific drop zone, including their payloads and reference dictionary.
-        /// </summary>
-        public void Clear(string dropZoneName)
-        {
-            DropZoneList.Remove(dropZoneName);
-            lock (lockPoint)
-            {
-                foreach (var clientEntry in ClientWatchList)
-                {
-                    if (clientEntry.AccessPoints.ContainsKey(dropZoneName))
-                    {
-                        clientEntry.AccessPoints.Remove(dropZoneName);
-                    }
-                }
-            }
-        }
+		private string ResolveLocalSpec(string localFolderSpec)
+		{
+			string result = localFolderSpec;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				result = result.Replace(@"/", @"\").ResolvePathPlaceholders();
+				result = result.Replace("$HOME", Environment.GetEnvironmentVariable("USERPROFILE"));
+				while (result[result.Length - 1] == '\\') result = result.Substring(0, result.Length - 1);
+				result += "\\";
+			}
+			else
+			{
+				result = result.Replace("$HOME", Environment.GetEnvironmentVariable("HOME"));
+				while (result[result.Length - 1] == '/') result = result.Substring(0, result.Length - 1);
+				result += "/";
+			}
+			return result;
+		}
 
-        /// <summary>
-        /// Shutdowns down the web server, requiring restart at the command line.
-        /// </summary>
-        public void Shutdown()
-        {
-            // triggers a timer, which does the actual shutdown after the thread is in an idle state.
-            stopTimer.Enabled = true;
-        }
+		/// <summary>
+		/// Reads the content from a file, using the key as the file name.
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public BlobState ReadBlob(string zoneName, string key, out StoredValue value)
+		{
+			value = null;
+			var zoneFolder = Path.Combine(PersistBaseFolder, zoneName);
+			if (!Directory.Exists(zoneFolder)) return BlobState.NotFound;
 
-        private static void OnTimedEvent(object source, ElapsedEventArgs e)
-        {
-            System.Environment.Exit(0);
-        }
-    }
+			var filename = Path.Combine(zoneFolder, key);
+			value = ObjectJsonSerializer<StoredValue>.LoadDocumentFormat(filename);
+			return BlobState.Exists;
+		}
+
+		/// <summary>
+		/// Creates a file with the content, using the key as the file name
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public BlobState SaveBlob(string zoneName, string key, StoredValue value)
+		{
+			var zoneFolder = Path.Combine(PersistBaseFolder, zoneName);
+			if (!Directory.Exists(zoneFolder)) Directory.CreateDirectory(zoneFolder);
+
+			var filename = Path.Combine(zoneFolder, key);
+			ObjectJsonSerializer<StoredValue>.SaveDocumentFormat(value, filename);
+			return BlobState.Exists;
+		}
+
+		/// <summary>
+		/// Deletes a file containing the content, using the key as the file name
+		/// </summary>
+		/// <param name="zoneName"></param>
+		/// <param name="key"></param>
+		public void DeleteBlob(string zoneName, string key)
+		{
+			var zoneFolder = Path.Combine(PersistBaseFolder, zoneName);
+			if (!Directory.Exists(zoneFolder)) return;
+
+			var filename = Path.Combine(zoneFolder, key);
+			if (!File.Exists(filename)) return;
+
+			File.Delete(filename);
+		}
+
+		/// <summary>
+		/// Clear all drop zones, including their payloads and reference dictionary.
+		/// </summary>
+		public void Reset()
+		{
+			foreach (var zoneName in DropZoneList.Keys)
+			{
+				Clear(zoneName);
+			}
+			DropZoneList.Clear();
+			ClientWatchList.Clear();
+		}
+
+		/// <summary>
+		/// Clear a specific drop zone, including their payloads and reference dictionary.
+		/// </summary>
+		public void Clear(string zoneName)
+		{
+			var zoneFolder = Path.Combine(PersistBaseFolder, zoneName);
+			if (Directory.Exists(zoneFolder)) Directory.Delete(zoneFolder);
+			DropZoneList.Remove(zoneName);
+			lock (lockPoint)
+			{
+				foreach (var clientEntry in ClientWatchList)
+				{
+					if (clientEntry.AccessPoints.ContainsKey(zoneName))
+					{
+						clientEntry.AccessPoints.Remove(zoneName);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Shutdowns down the web server, requiring restart at the command line.
+		/// </summary>
+		public void Shutdown()
+		{
+			// triggers a timer, which does the actual shutdown after the thread is in an idle state.
+			stopTimer.Enabled = true;
+		}
+
+		private static void OnTimedEvent(object source, ElapsedEventArgs e)
+		{
+			System.Environment.Exit(0);
+		}
+	}
 }
