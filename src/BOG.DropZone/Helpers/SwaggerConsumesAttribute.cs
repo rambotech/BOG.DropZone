96478044c16cfbe101c5e8711700ea5859b0f8e7
John J Schultz <rambotech@users.noreply.github.com>
2023-08-26 15:31:53 -0400

    v2.4.2

---
+++ src/BOG.DropZone/Helpers/SwaggerConsumesAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace BOG.DropZone.Helpers
+{
+	/// <summary>
+	/// Needed for controlling the Request Body dropdown content in the Swagger display.
+	/// </summary>
+	
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SwaggerConsumesAttribute : Attribute
+	{
+		public SwaggerConsumesAttribute(params string[] contentTypes)
+		{
+			this.ContentTypes = contentTypes;
+		}
+
+		public IEnumerable<string> ContentTypes { get; }
+	}
+}
